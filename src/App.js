e115a4b218d2161cc6099f31186d949e478e8d2f
baedollee <baesee0401@gmail.com>
2022-08-13 13:28:41 +0900

    fix: bundleErros

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const App = () => {
+  const posts = useSelector((state) => state.posts.posts);
+  console.log(posts);
   return <div>App</div>;
 };
 
